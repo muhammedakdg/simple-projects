1d5a338aca4a2633a7429b3cb9868da8b8e2aae4
Muhammed Emin <168139141+muhammedakdg@users.noreply.github.com>
2024-11-08 11:46:43 -0500

    Delete lab3_task3.py

--- lab3_task3.py
+++
@@ -1,8 +0,0 @@
-# get input
-age = int(input("What is the child's age? "))
-height = int(input("What is the child's height? "))
-
-# you can add any line of code between this line and the line with return
-
-# show result
-print(height > 120 or age > 6) # You should modify this line as needed
